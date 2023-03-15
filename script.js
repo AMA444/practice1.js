1283c1d367b4c513b799a86cce8945834449d2b9
AMA444 <107946484+AMA444@users.noreply.github.com>
2023-03-15 17:04:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+/* function letterfinder(word,match)
+ {
+    for(var i=0;i<word.length;i++){
+        if(word[i]==match){
+            console.log('found the match')
+        }
+        else{
+            console.log('no match')
+        }
+    }
+ }
+ 
+var group ={
+    name:'ahmad',
+    age :20,
+    hight :5.4,
+}
+console.log(group)
+console.log(group.name)
+group.class =10;
+console.log(group)
+group["fulname"]="muhamad";
+console.log(group.fulname)
+*/
+
+/*var person ={
+    first :'ahmad',
+    second :'ali',
+    age :20,
+    }
+
+
+for(var i=0;i<arr.length;i++){
+    console.log(person[arr[i]]);
+}
+console.log(person['first']);
+*/
+/*var arr =['first' ,'second','age'];
+arr.push(30);
+console.log(arr);
+arr.pop();
+arr.pop();
+arr.pop();
+console.log(arr);*/
+/*var arrbilder(one,two,three )
+ {
+    var arr =[];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+ return arr;
+}
+arrbilder('alii','muhammad',20);*/
+/*var decimal = Math.ceil(5.1);
+console.log(decimal);*/
+var decimal = Math.random()*10
+;
+console.log(decimal);
+var result = Math.ceil(decimal);
+console.log(result);
+var firstname ='hello ';
+var secondname ='Kitty';
+console.log (firstname + secondname);
